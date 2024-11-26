ac34f7612e24ec5d6e98e637b0dc95d4cee3f493
Federico Menegoz <menegoz.federico@gmail.com>
2024-11-26 17:34:28 +0100

    need to fix resizing

--- src/Background/systems/Resizer.js
+++ src/Background/systems/Resizer.js
@@ -1,12 +1,15 @@
+const PADDINGX = 24
+const PADDINGY = 32
 class Resizer {
   constructor(container, camera, renderer, css3dRenderer, cube, contentDiv) {
     // set the initial size
     setSize(container, camera, renderer, css3dRenderer)
-    setCubeSize(container, cube, contentDiv)
+    fitDivToCube(container, cube, contentDiv)
+    scaleToContainer(container, cube, PADDINGX, PADDINGY)
 
     window.addEventListener('resize', () => {
       setSize(container, camera, renderer, css3dRenderer)
-      setCubeSize(container, cube, contentDiv)
+      scaleToContainer(container, cube, PADDINGX, PADDINGY)
     })
   }
 }
@@ -29,15 +32,28 @@ const setSize = (container, camera, renderer, css3dRenderer) => {
   renderer.setPixelRatio(window.devicePixelRatio)
 }
 
-const setCubeSize = (container, cube, contentDiv3d) => {
-  const paddingX = 24
-  const paddingY = 32
-  scaleToContainer(container, cube, paddingX, paddingY, contentDiv3d)
-}
-
 export { Resizer }
 
-function scaleToContainer(container, node, paddingX, paddingY, contentDiv3d) {
+function scaleToContainer(container, node, paddingX, paddingY) {
+  const CONTAINER_WIDTH = container.clientWidth
+  const CONTAINER_HEIGHT = container.clientHeight
+  // Compute the bounding box of the cube
+  node.geometry.computeBoundingBox()
+  const boundingBox = node.geometry.boundingBox
+  const currentWidth = (boundingBox.max.x - boundingBox.min.x)
+  const currentHeight = (boundingBox.max.y - boundingBox.min.y)
+
+  // Fit the cube correctly in the screen
+  const actualWidth = CONTAINER_WIDTH - paddingX * 2
+  const actualHeight = CONTAINER_HEIGHT - paddingY * 2
+  const scaleX = actualWidth / currentWidth
+  const scaleY = actualHeight / currentHeight
+  node.scale.set(scaleX, scaleY, scaleX)
+  node.position.z = -actualWidth / 2
+
+}
+
+function fitDivToCube(container, node, contentDiv3d) {
   const CONTAINER_WIDTH = container.clientWidth
   const CONTAINER_HEIGHT = container.clientHeight
   // Compute the bounding box of the cube
@@ -46,7 +62,7 @@ function scaleToContainer(container, node, paddingX, paddingY, contentDiv3d) {
   const currentDepth = boundingBox.max.z - boundingBox.min.z
   const currentWidth = (boundingBox.max.x - boundingBox.min.x)
   const currentHeight = (boundingBox.max.y - boundingBox.min.y)
-  
+
   // Put the infoDiv into the face of the cube
   contentDiv3d.position.z = currentDepth / 2
 
@@ -59,13 +75,4 @@ function scaleToContainer(container, node, paddingX, paddingY, contentDiv3d) {
   const scaleXDiv = currentWidth / currentWidthDiv
   const scaleYDiv = currentHeight / currentHeightDiv
   contentDiv3d.scale.set(scaleXDiv, scaleYDiv, 1)
-
-  // Fit the cube correctly in the screen
-  const actualWidth = CONTAINER_WIDTH - paddingX * 2
-  const actualHeight = CONTAINER_HEIGHT - paddingY * 2
-  const scaleX = actualWidth / currentWidth
-  const scaleY = actualHeight / currentHeight
-  node.scale.set(scaleX, scaleY, scaleX)
-  node.position.z -= actualWidth / 2
-
 }
